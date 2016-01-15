15c3016b9ceaae80b1245f8894e6241f7335f738
Richard Kersey <richard@razorit.com>
2016-01-15 14:08:58 -0500

    ignore

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello big World");
+  response.write("Hello biggie World");
   response.end();
 }).listen(process.env.PORT || 8888);
