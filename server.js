820e65dea18a3ae66ace560d6164b23b64646e4e
Richard Kersey <richard@razorit.com>
2016-01-15 13:24:43 -0500

    a small edit

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello ddd World");
+  response.write("Hello big World");
   response.end();
 }).listen(process.env.PORT || 8888);
